12f88f8aaba689075acb7fca35cccf927abbc256
luca-gif <luca.89m@gmail.com>
2022-04-08 18:51:32 +0200

    Milestone 3

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 for (let i = 1; i <= 100; i++) {
     if (i % 3 === 0) {
-        // il numero è divisibile per tre
         console.log("Fizz");
     } else if (i % 5 === 0) {
         console.log("Buzz");
@@ -13,7 +12,21 @@ for (let i = 1; i <= 100; i++) {
 
     div.className = "box";
 
-    div.append("Hello!");
-
     container.append(div);
+
+    if (i % 3 === 0) {
+        div.classList.add("yellow");
+        div.append("Fizz");
+    } else if (i % 5 === 0) {
+        div.classList.add("red");
+        div.append("Buzz");
+    } else {
+        div.append(i);
+    }
+
+    /* Questo codice nell' Else If non mi funzionava, non capisco il motivo */
+
+    if (i % 3 === 0 && i % 5 === 0) {
+        div.classList.add("violet");
+    }
 }
